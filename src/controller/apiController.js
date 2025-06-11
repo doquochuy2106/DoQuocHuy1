64e50cf47600928c68158964c37187d9b350eeeb
unknown <huy.do02062004@gmail.com>
2025-06-12 01:46:28 +0700

    video 10.2.1 API Register

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -1,3 +1,5 @@
+import loginRegisterService from "../service/loginRegisterService.js"
+
 const testAPI = (req, res) => {
     return res.status(200).json({
         message: "ok",
@@ -5,7 +7,41 @@ const testAPI = (req, res) => {
     })
 }
 
-const handleRegister = (req, res) => {
+const handleRegister = async (req, res) => {
+    try {
+        //req.body: email, phone, username, password
+        if (!req.body.email || !req.body.phone || !req.body.password) {
+            return res.status(200).json({
+                EM: 'Missing required parameters',// error message
+                EC: '1', //error code
+                DT: '',//date   
+            })
+        }
+        if (req.body.password && req.body.password.length < 4) {
+            return res.status(200).json({
+                EM: 'Your password must have more than 3 letter',// error message
+                EC: '1', //error code
+                DT: '',//date   
+            })
+        }
+
+        //service: create user
+        let data = await loginRegisterService.registerNewUser(req.body)
+
+        return res.status(200).json({
+            EM: data.EM,// error message
+            EC: data.EC, //error code
+            DT: '',//date   
+        })
+
+    } catch (e) {
+        return res.status(500).json({
+            EM: 'error from server',// error message
+            EC: '-1', //error code
+            DT: '',//date   
+        })
+    }
+
     console.log(">>>Call me", req.body)
 }
 
