dba548e8b7f9f93a91e9299336924e5e61d437d5
unknown <huy.do02062004@gmail.com>
2025-06-23 20:37:16 +0700

    video 10.5 Base User CRUD

---
+++ src/controller/userController.js
@@ -0,0 +1,64 @@
+import userApiService from "../service/userApiService.js"
+
+const readFunc = async (req, res) => {
+    try {
+        let data = await userApiService.getAllUser();
+
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT
+        })
+
+    } catch (error) {
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '1',
+            DT: ""
+        })
+    }
+}
+
+const createFunc = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '1',
+            DT: ""
+        })
+    }
+}
+
+const updateFunc = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '1',
+            DT: ""
+        })
+    }
+}
+
+const deleteFunc = (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(err);
+        return res.status(500).json({
+            EM: 'error from server',
+            EC: '1',
+            DT: ""
+        })
+    }
+}
+
+module.exports = {
+    readFunc, createFunc, updateFunc, deleteFunc
+}
