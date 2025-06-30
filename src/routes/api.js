9ae2036925829bbaba5ee520018c093935a46974
unknown <huy.do02062004@gmail.com>
2025-06-30 11:53:32 +0700

    video 10.6.4 Modal Create User - Tạo Form Thêm Mới User

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,7 @@
 import express from "express"
 import apiController from "../controller/apiController.js"
 import userController from "../controller/userController.js"
+import groupController from "../controller/groupController.js"
 
 const router = express.Router();
 
@@ -20,6 +21,8 @@ const initApiRoutes = (app) => {
     router.post("/user/create", userController.createFunc);
     router.put("/user/update", userController.updateFunc);
     router.delete("/user/delete", userController.deleteFunc);
+
+    router.get("/group/read", groupController.readFunc);
 
     return app.use("/api/v1", router)
 }
