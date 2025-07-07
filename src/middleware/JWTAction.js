cedb5c655ccf0d40875cef9bcaaf1ae550aa15ac
unknown <huy.do02062004@gmail.com>
2025-07-07 11:35:24 +0700

    video 11.2 How To Use JWT ? Sử Dụng JWT

---
+++ src/middleware/JWTAction.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+import jwt from 'jsonwebtoken';
+
+const createJWT = () => {
+    let payload = { name: 'doquochuy', address: 'ho chi minh' }
+    let key = process.env.JWT_SECRET
+    let token = null
+    try {
+        token = jwt.sign(payload, key);
+        console.log(token)
+    } catch (error) {
+        console.log(error)
+    }
+
+    return token;
+}
+
+const verifyToken = (token) => {
+    let key = process.env.JWT_SECRET;
+    let data = null;
+    try {
+        let decoded = jwt.verify(token, key);
+        data = decoded
+    } catch (error) {
+        console.log(err);
+    }
+    return data;
+}
+
+module.exports = {
+    createJWT, verifyToken
+}
