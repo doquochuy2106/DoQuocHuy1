fa69081f0bb3cb1bfd30534e2a279f5d61c811f3
unknown <huy.do02062004@gmail.com>
2025-07-14 11:11:55 +0700

    video 11.7 Middleware Routes - Check Quyền Cho Tất Cả Routes

--- src/middleware/JWTAction.js
+++ src/middleware/JWTAction.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 import jwt from 'jsonwebtoken';
 
+const nonSecurePaths = ['/', '/login', 'register'];
+
+
+
 const createJWT = (payload) => {
     let key = process.env.JWT_SECRET
     let token = null
@@ -25,6 +29,7 @@ const verifyToken = (token) => {
 }
 
 const checkUserJWT = (req, res, next) => {
+    if (nonSecurePaths.includes(req.path)) return next();
     let cookies = req.cookies;
     if (cookies && cookies.jwt) {
         let token = cookies.jwt
@@ -52,6 +57,8 @@ const checkUserJWT = (req, res, next) => {
 }
 
 const checkUserPermission = (req, res, next) => {
+    if (nonSecurePaths.includes(req.path)) return next();
+
     if (req.user) {
         let email = req.user.email;
         let roles = req.user.groupWithRoles.Roles;
