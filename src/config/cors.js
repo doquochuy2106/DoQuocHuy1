a25b8dea7f65c35b3db5ae3a01c1117578db76c3
unknown <huy.do02062004@gmail.com>
2025-07-17 11:13:26 +0700

    video 11.11 Bearer Authentication - Xác Thực Với Header Token

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,6 +4,7 @@ dotenv.config();
 const configCors = (app) => {
     //Add header before the routes are defined
     app.use(function (req, res, next) {
+        console.log(req.method);
         //website you wish to allow to connnect
         res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
 
@@ -11,12 +12,15 @@ const configCors = (app) => {
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATH, DELETE')
 
         //Request header you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Request-with,content-type');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Request-with,content-type, Authorization');
 
         //Set to true if you need the website include cookies in the  request sent
         //to the API(e.g. in case you use session)
         res.setHeader('Access-Control-Allow-Credentials', true);
 
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(200);
+        }
         //Pass to next layer of middleware
         next();
     });
